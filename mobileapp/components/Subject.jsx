f7a7afbb3235f7516199d7f2b4e9a328b12f4978
Disitha <disithar@gmail.com>
2025-01-04 20:57:32 +0530

    import headers, logos and footers

--- mobileapp/components/Subject.jsx
+++ mobileapp/components/Subject.jsx
@@ -1,10 +1,26 @@
 import { StyleSheet,View,ScrollView } from "react-native";
 import { PaperProvider, Text } from "react-native-paper";
+import Header from "./Header";
+import Logo from "./Logo";
+import {subjects} from "../data/StudentsDb";
+import {marks} from "../data/StudentsDb";
+
 export default function Subject() {
+
+  const {user} = route.params;
+
+  const subjectdetails = subjects.find(
+    
+  )
+
   return (
     <PaperProvider>
         <ScrollView>
             <View style={styles.subject}>
+              <View>
+                <Header/>
+                <Logo/>
+              </View>
                 <Text>subject</Text>
             </View>
         </ScrollView>
