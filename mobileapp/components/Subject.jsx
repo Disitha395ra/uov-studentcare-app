ea125e228cb924a64f280dde6469757c47582811
Disitha <disithar@gmail.com>
2025-01-04 21:40:23 +0530

    course mapping

--- mobileapp/components/Subject.jsx
+++ mobileapp/components/Subject.jsx
@@ -4,12 +4,16 @@ import Header from "./Header";
 import Logo from "./Logo";
 import {subjects} from "../data/StudentsDb";
 import {marks} from "../data/StudentsDb";
+import { courses } from "../data/StudentsDb";
 
 export default function Subject({route,navigation}) {
 
   const {user} = route.params;
   const subjectdetails = subjects.find(course => user.course_id === course.id);
   const markdetails = marks.find(mark => user.id === mark.student_id);
+  const coursedetails = courses.find(
+    course=>user.course_id === course.id
+  )
 
   return (
     <PaperProvider>
